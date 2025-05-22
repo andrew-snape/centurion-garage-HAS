78b30e760288f6d9beb8d7d9c9e7c2ee73779b6c
Andrew Snape <8685028+andrew-snape@users.noreply.github.com>
2025-05-22 12:37:34 +1000

    Add files via upload
    Prepare version 0.9.1 for HACS compliance:
    
    - Bumped version in manifest.json
    - Added issue_tracker to manifest
    - Fixed hacs.json (removed invalid keys)
    - Added placeholder 256x256 icon.png (brands requirement)
    - Updated README with installation and usage info

--- custom_components/centurion/__init__.py
+++ custom_components/centurion/__init__.py
@@ -1,18 +1,1 @@
-from .const import DOMAIN
-import asyncio
-
-PLATFORMS = ["cover", "camera", "switch"]
-
-async def async_setup_entry(hass, config_entry):
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
-    return True
-
-async def async_unload_entry(hass, config_entry):
-    return all(
-        await asyncio.gather(
-            *[hass.config_entries.async_forward_entry_unload(config_entry, platform) for platform in PLATFORMS]
-        )
-    )
+# init
