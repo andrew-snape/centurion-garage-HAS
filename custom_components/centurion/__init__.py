20076b3fa8e0a853f4bda5c3f213f4eb1ea43bb6
Samit <42769125+urwrstkn8mare@users.noreply.github.com>
2026-02-24 23:07:02 +0800

    hotfixes

--- custom_components/centurion/__init__.py
+++ custom_components/centurion/__init__.py
@@ -1,18 +1,14 @@
-from .const import DOMAIN
 import asyncio
+
+from .const import DOMAIN
 
 PLATFORMS = ["cover", "switch"]
 
+
 async def async_setup_entry(hass, config_entry):
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
+
 async def async_unload_entry(hass, config_entry):
-    return all(
-        await asyncio.gather(
-            *[hass.config_entries.async_forward_entry_unload(config_entry, platform) for platform in PLATFORMS]
-        )
-    )
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
